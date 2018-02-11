55718cafc98e2874452969c4b53ae84d7a9b052f
solovovmb <solovov.mark@gmail.com>
2018-02-11 03:49:26 +0300

    change some column names

--- base/dbaccess.py
+++ base/dbaccess.py
@@ -20,45 +20,48 @@ add_drone = ("""
         drone_id) 
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """)
-data_drone = ('drone 001', 'mybrand', 'copter', 'something', 'ready', 'both', 
-              'joy', 'expert', 'adult', date_time, date_time, unic_id
+data_drone = ('drone 00+', 'brand name', 'copter', 'manufacturer + country', 
+        'ready', 'both', 'joy', 'expert', 'adult', 
+        date_time, date_time, unic_id
         )
 cursor.execute(add_drone, data_drone)
 
 add_sale = ("""
         INSERT INTO sale (price, provider_name, dron_full_name, descr, 
         specification, shipping_info, customer_rating, number_of_reviews, 
-        number_of_questions, drone_article, url, sale_fk)
+        number_of_questions, drone_article, url, sale_id)
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """)
 
 data_sale = (
-        '350.02', 'amazon', 'Bazzz ooo', 'so en so...', 
-        'some more tech detailes', 'dimensions weight destination', 2, 567, 
-        300, 'xz', 'amazon.com', unic_id
+        '350.02', 'amazon', 'Bazzz ooo', 'it flies and makes video', 
+        'tech detailes', 'dimensions weight sale abroad', 5, 600, 
+        300, 'Amazon_ID', 'amazon.com', unic_id
         )
                      
 cursor.execute(add_sale, data_sale)
 
-add_reviwe = ("""
+add_review = ("""
         INSERT INTO review (author_rate, title, text, author, 
-        number_of_comments, review_date, review_fk)
+        number_of_comments, review_date, review_id)
         VALUES (%s, %s, %s, %s, %s, %s, %s)
         """)
 
-data_reviwe = (5, 'zzz', 'once upon a time', 'alexander', 
-               500, '2018-02-09 16:26:35', unic_id
-               )
+data_review = (5, 'title text', 'text of review', 'alexander', 
+        5, '2018-02-09 16:26:35', unic_id
+        )
                      
-cursor.execute(add_reviwe, data_reviwe)
+cursor.execute(add_review, data_review)
 
 # Make sure data is committed to the database
 cnx.commit()
 
 # Last row output
 sql = ("""
-       SELECT * FROM drone
-       ORDER BY drone.id DESC LIMIT 1
+       SELECT * FROM drone join sale join review
+       on  drone.drone_id = sale.sale_id
+       where drone.drone_id = review.review_id
+       ORDER BY drone.updated_at DESC LIMIT 1
        """)
 
 cursor.execute(sql)
